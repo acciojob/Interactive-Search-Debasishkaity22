04b7da41b34af31ffc4c78b3e63fa2b570e40097
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-26 16:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn=document.querySelector(".btn");
+btn.addEventListener("click",function(){
+	let devSearch=document.querySelector(".search");
+	devSearch.classList.add("active");
+
+	let input1=document.querySelector(".input");
+	input1.focus();
+})
